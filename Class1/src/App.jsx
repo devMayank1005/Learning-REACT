08f1b9114dbe59e723d44c4b45dce9266abda885
devMayank1005 <devmayank1005@gmail.com>
2025-12-22 16:54:55 +0530

    set up tailwind with vite

--- Class1/src/App.jsx
+++ Class1/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Nav from './components/Nav'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -31,5 +31,14 @@ function App() {
     </>
   )
 }
+const AppWithNav = () => {
+  return (
+    <>
+      <Nav />
+      <App />
+    </>
+  )
+}
+
 
 export default App
---
+++ Class1/src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+      this is Nav Bar
+    </div>
+  )
+}
+
+export default Nav
