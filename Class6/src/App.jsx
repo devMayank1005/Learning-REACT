3354231ed6bb6c81cc1e78d568288f560d2ca17b
devMayank1005 <devmayank1005@gmail.com>
2026-01-02 18:37:47 +0530

    2 way binidng

--- Class6/src/App.jsx
+++ Class6/src/App.jsx
@@ -1,22 +1,34 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [num, setNum] = useState('');
+   const [username, setUsername] = useState('');
+   const [allUsers, setAllUsers] = useState([]);
 
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log("Number:", num, "Submitted"); // handle form submission logic here
+    const newAllUsers = [...allUsers, username];
+
+    
+    console.log(newAllUsers);
+     
+    setAllUsers(newAllUsers);
+  
+    console.log("Number:", username, "Submitted"); // handle form submission logic here
+
+    setUsername(''); // Clear the input field after submission
   };
+ 
+
 
   return (
     <div>
-      <form onSubmit={(e) => {
-        setNum(e.target[0].value);
-        submitHandler(e);
-      }}>
+      <form onSubmit={submitHandler}>
         <input
           type="text"
           placeholder="Enter a number"
+          value={username}
+          required
+          onChange={(e) => setUsername(e.target.value)}
         />
         <button type="submit">Submit</button>
       </form>
