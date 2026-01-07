c1097b90b876c8bae1208422dfeb232073fb2692
devMayank1005 <devmayank1005@gmail.com>
2026-01-08 01:17:48 +0530

    React Route

---
+++ Class11/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1 className='text-3xl font-bold absolute top-1/2 left-1/2 -translate-1/2'>Home page</h1>
+    </div>
+  )
+}
+
+export default Home
